2387c2c5934e3db77cd4e8e5c4388c216e541969
Maxytomay <osimayungbo@gmail.com>
2024-08-09 07:48:45 +0100

    Read standard input

--- editor.c
+++ editor.c
@@ -1,3 +1,8 @@
+#include <unistd.h>
+
 int main() {
-  return 0;
+    char c;
+    // Read from standard input byte by byte, enter 'q' to quit
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    return 0;
 }
