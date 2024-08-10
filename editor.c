86304ef4f25a30d4c424cc0f63ef1e2492c095a6
Maxytomay <osimayungbo@gmail.com>
2024-08-10 05:40:42 +0100

    More features and error handling

--- editor.c
+++ editor.c
@@ -3,35 +3,62 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <errno.h>
 
 struct termios orig_termios;
 
+void crash(const char *s) {
+    // Print descriptive error
+    perror(s);
+    exit(1);
+}
+
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) crash("tcsetattr");
 }
 
 void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) crash("tcgetattr");
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO | ICANON);
+    // Disable Ctrl-M, Ctrl-S and Ctrl-Q
+    raw.c_lflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    // Disable logging new line
+    raw.c_oflag &= ~(OPOST);
+
+    // Set character size to 8 bits per length
+    raw.c_cflag |= (CS8);
+
+    // Disable X, X, Ctrl-V, X
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+    // minimum number of bytes of input needed before read() can return
+    raw.c_cc[VMIN] = 0;
+
+    // maximum amount of time to wait before read() returns
+    raw.c_cc[VTIME] = 1;
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) crash("tcsetattr");
 }
 
 int main() {
     enableRawMode();
 
-    char c;
     // Read from standard input byte by byte, enter 'q' to quit
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    while (1) {
+         char c = '\0';
+         if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) crash("read");
+
         // Only print out printable characters
         if (iscntrl(c)) {
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         } else {
-            printf("%d ('%c')\n", c, c);
+            printf("%d ('%c')\r\n", c, c);
         }
+        if (c == 'q') break;
     }
+
     return 0;
 }
