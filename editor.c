a06f6358adc950e26fbb3b73c6336f6f9f661ed8
Maxytomay <osimayungbo@gmail.com>
2024-08-10 20:37:51 +0100

    Add comments

--- editor.c
+++ editor.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <errno.h>
 
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 struct termios orig_termios;
 
 void crash(const char *s) {
@@ -14,24 +16,32 @@ void crash(const char *s) {
 }
 
 void disableRawMode() {
+    // Check if tcsetattr() returns -1 (when an error occurs) and return error message
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) crash("tcsetattr");
 }
 
 void enableRawMode() {
     if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) crash("tcgetattr");
+
+    // Register function that executes when the program exits normally
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
-    // Disable Ctrl-M, Ctrl-S and Ctrl-Q
+    // c_lflag contains various local mode flags that control terminal behaviour
+    // &= ~ (Bitwise AND with a NOT to flip specific switches in the c_lflag)
+
+    // Disable: Interrupt signals (Ctrl-C), parity checking, stripping of high-order bits, carriage return mapping (Ctrl-M), software flow control (Ctrl-S and Ctrl-Q)
     raw.c_lflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 
-    // Disable logging new line
+    // c_oflag controls output processing flags for the terminal
+    // Disable logging new line (output processing)
     raw.c_oflag &= ~(OPOST);
 
+    // c_cflag controls hardware and data format settings for terminal I/O
     // Set character size to 8 bits per length
     raw.c_cflag |= (CS8);
 
-    // Disable X, X, Ctrl-V, X
+    // Disable: input characters echoed to screen, input processed line-by-line, Ctrl-V, Special characters (Ctrl-Z) generating signals
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
     // minimum number of bytes of input needed before read() can return
@@ -48,8 +58,9 @@ int main() {
 
     // Read from standard input byte by byte, enter 'q' to quit
     while (1) {
-         char c = '\0';
-         if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) crash("read");
+        char c = '\0';
+        // Error handling
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) crash("read");
 
         // Only print out printable characters
         if (iscntrl(c)) {
@@ -57,7 +68,7 @@ int main() {
         } else {
             printf("%d ('%c')\r\n", c, c);
         }
-        if (c == 'q') break;
+        if (c == CTRL_KEY('q')) break;
     }
 
     return 0;
